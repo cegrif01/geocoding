4ff1863877043fbd79927fbc1b5a77d89abceeb4
Charles Griffin <cegrif01@gmail.com>
2024-08-14 19:57:03 -0400

    added phpunit version that's compatible with my php8.2 dev version

---
+++ tests/Actions/ConvertAddressIntoLatAndLongActionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Actions;
+
+use PHPUnit\Framework\TestCase;
+
+//vendor/bin/phpunit tests/Actions/ConvertAddressIntoLatAndLongActionTest.php
+class ConvertAddressIntoLatAndLongActionTest extends TestCase
+{
+
+    public function test_can_convert_address_into_lat_and_long()
+    {
+        $this->assertTrue(true);
+    }
+
+}
