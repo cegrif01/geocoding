722b83e856b4b426a4f645cb86714b7fd5255630
Charles Griffin <cegrif01@gmail.com>
2024-08-19 19:53:13 -0400

    validation is complete and all tests pass.  Now let's move on to the summary of the blog

--- src/Domain/LatLong.php
+++ src/Domain/LatLong.php
@@ -10,9 +10,13 @@ class LatLong
 {
     private readonly LatLongStruct $latLongStruct;
 
+    /**
+     * @throws InvalidLatitudeAndLongitudeException
+     */
     public function __construct(string $latitude, string $longitude)
     {
         $this->latLongStruct = new LatLongStruct(latitude: $latitude, longitude: $longitude);
+
         $this->validate();
     }
 
@@ -22,8 +26,16 @@ class LatLong
             throw new InvalidLatitudeAndLongitudeException($this->getLatitude(). ' is not a valid latitude');
         }
 
-        if(!preg_match('//^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$//',$this->getLongitude())) {
+        if(!preg_match('/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/',$this->getLongitude())) {
             throw new InvalidLatitudeAndLongitudeException($this->getLongitude(). ' is not a valid longitude');
+        }
+
+        if( !(($this->getLatitude() >= -90) && ($this->getLatitude() <= 90)) ) {
+            throw new InvalidLatitudeAndLongitudeException($this->getLatitude() . ' is an invalid latitude coordinate');
+        }
+
+        if( !(($this->getLongitude() >= -180) && ($this->getLongitude() <= 180)) ) {
+            throw new InvalidLatitudeAndLongitudeException($this->getLongitude() . ' is an invalid longitude coordinate');
         }
     }
 
